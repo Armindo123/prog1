93828baffa73bcbc08f0a77e33b2ae842ec4cbd1
Carlos <l.l_c@live.com.pt>
2018-03-13 15:26:45 +0000

    1a parte

---
+++ aula/fp2/aula2ex1-3.py
@@ -0,0 +1,6 @@
+#calculadora de dois numeros
+
+num1 = int(input("Introduza um numero: "))
+num2 = int(input("Introduza outro numero: "))
+operacao = input("Introduza que operacao deseja fazer (+ - * ou /): ")
+

283c5e15d5373a1ab0297239c923b47aa000115e
Carlos <l.l_c@live.com.pt>
2018-03-13 15:39:21 +0000

    ex3fim

--- aula/fp2/aula2ex1-3.py
+++ aula/fp2/aula2ex1-3.py
@@ -1,8 +1,9 @@
 #calculadora de dois numeros
 
 num1 = int(input("Introduza um numero: "))
-num2 = int(input("Introduza outro numero: "))
 operacao = input("Introduza que operacao deseja fazer (+ - * ou /): ")
+num2 = int(input("Introduza outro numero: "))
+
 
 if operacao == "+":
     print(num1 + num2)

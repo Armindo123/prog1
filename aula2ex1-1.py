db2fb12e2690d90473d843a20de779d66aefad6d
Carlos <l.l_c@live.com.pt>
2018-03-13 14:30:40 +0000

    adicionado

---
+++ aula2ex1-1.py
@@ -0,0 +1,14 @@
+#maior de dois numeros inteiros
+print("Indique dois numeros separados por um espaco")
+num1, num2 = input("Escreva os dois numeros escolhidos: ").split()
+
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 == num2:
+    print("Os numeros introduzidos sao iguais; logo, nao e possivel indicar o maior.")
+elif num1 > num2:
+    print("O maior numero e: {}").format(num1)
+else
+    print("O maior numero e: {}").format(num2)
+

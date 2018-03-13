b909ad8eb48ce642860677fe2f40588a32c49465
Carlos <l.l_c@live.com.pt>
2018-03-13 15:38:32 +0000

    ex3fi1

--- aula/fp2/aula2ex1-3.py
+++ aula/fp2/aula2ex1-3.py
@@ -4,3 +4,13 @@ num1 = int(input("Introduza um numero: "))
 num2 = int(input("Introduza outro numero: "))
 operacao = input("Introduza que operacao deseja fazer (+ - * ou /): ")
 
+if operacao == "+":
+    print(num1 + num2)
+elif operacao == "-":
+    print(num1 - num2)
+elif operacao == "*":
+    print(num1 * num2)
+elif operacao == "/":
+    print(num1 / num2)
+else:
+    print("Nao introduziu a operacao de forma correta")

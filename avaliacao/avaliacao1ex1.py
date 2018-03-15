269e4d84e1a07a2cc555c6e549b5328bc1c595f9
Carlos <l.l_c@live.com.pt>
2018-03-15 15:07:51 +0000

    versao final 1-1

---
+++ avaliacao/avaliacao1ex1.py
@@ -0,0 +1,10 @@
+#indicar o numero diretamente antes e depois do introduzido
+
+numero = int(input("Introduza um numero inteiro: "))
+
+
+
+antes = numero - 1
+depois = numero + 1
+print("Numero anterior: {}".format(antes))
+print("Numero postrior: {}".format(depois))

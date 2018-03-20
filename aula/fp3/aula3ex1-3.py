58e9e15b0f929470510ec269d0ab0494a1419c9d
Carlos <l.l_c@live.com.pt>
2018-03-20 14:01:28 +0000

    3feito

--- aula/fp3/aula3ex1-3.py
+++ aula/fp3/aula3ex1-3.py
@@ -1,2 +1,8 @@
 #apresentar os valores num intervalo escolhido com saltos escolhidos
 
+fim = int(input("Valor final do intervalo: "))
+incremento = int(input("Incremento escolhido: "))
+
+fim += 1
+for i in range(0, fim, incremento):
+    print(i, end=" ")

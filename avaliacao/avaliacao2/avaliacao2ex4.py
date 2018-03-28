17d6a1b31910b4cb612995d52f82113e56f1ed05
Carlos <l.l_c@live.com.pt>
2018-03-28 11:45:53 +0100

    fim ex4

---
+++ avaliacao/avaliacao2/avaliacao2ex4.py
@@ -0,0 +1,11 @@
+#verificar se o numero e divisivel por 3 e 5 ao mesmo tempo
+
+numero = int(input("Introduza um numero inteiro: "))
+
+if numero%3 == 0:
+    if numero%5 == 0:
+        print("{} e simultaneamente divisivel por 3 e 5".format(numero))
+    else:
+        print("{} nao e simultaneamente divisivel por 3 e 5".format(numero))
+else:
+    print("{} nao e simultaneamente divisivel por 3 e 5".format(numero))

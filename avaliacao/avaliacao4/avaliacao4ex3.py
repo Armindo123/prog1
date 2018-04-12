bdb038214c266a4c49f52dd2e5ba1283aa87d248
Carlos <l.l_c@live.com.pt>
2018-04-12 15:43:58 +0100

    fim ex3

---
+++ avaliacao/avaliacao4/avaliacao4ex3.py
@@ -0,0 +1,29 @@
+#quadrilatero
+
+def quadrilatero(linhas, caracter, colunas):
+    print(caracter * colunas)
+    baixo(linhas)
+    print(caracter * colunas)
+
+def baixo(linhas):
+    igual = caracter + vazio * (colunas - 2) + caracter
+    linhas -= 2
+    if linhas > 0:
+        
+        print(igual) 
+        linhas -= 1
+        baixo(linhas)
+        print(igual)
+    elif linhas <= 0:
+        exit
+
+
+
+vazio = str(" ")
+
+caracter = str(input("Introduza o caracter desejada: "))
+linhas = int(input("Introduza o numero de linhas desejado: "))
+colunas = int(input("Introduza o numero de colunas desejado: "))
+
+
+quadrilatero(linhas, caracter, colunas)

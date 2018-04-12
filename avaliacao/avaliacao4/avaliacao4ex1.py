d68d4c0c1876cb563de3d9faefc4e1931c72647d
Carlos <l.l_c@live.com.pt>
2018-04-12 14:02:35 +0100

    fim ex2

--- avaliacao/avaliacao4/avaliacao4ex1.py
+++ avaliacao/avaliacao4/avaliacao4ex1.py
@@ -3,14 +3,29 @@
 def username():
     for a in range(1, 4):
         
-        default1 = "abc"
+        defaultusername = "defaultusername"
         username1 = input("Introduza o seu username: ")
-        if username1 == default1:
-            print("Parabens")
+        if username1 == defaultusername:
+            password()
+            break
         else:
-            
+            print("Username errado.")
             if a == 3:
                 print("Tentativas esgotadas")
+
+def password():
+    for b in range(1,4):
+
+        defaultpassword = "defaultpassword"
+        password1 = input("Introduza a sua password: ")
+        if password1 == defaultpassword:
+            print("Login efetuado com sucesso")
+            break
+        else:
+            print("Password errada.")
+            if b == 3:
+                print("Tentativas esgotadas")
+
 
 
 username()
---
+++ avaliacao/avaliacao4/avaliacao4ex2.py
@@ -0,0 +1,20 @@
+#conversao E-D D-E
+
+def currency():
+    a, b = input("Introduza uma quantia e D ou E separando com um espaco: ").split()
+    a = float(a)
+    b = str(b)
+    if b == "D":
+        convertorDE(a)
+        print("{} Dolars correspondem a {} Euros".format(a, convertorDE(a)))
+    elif b == "E":
+        convertorED(a)
+        print("{} Euros correspondem a {} Dolars".format(a, convertorED(a)))
+def convertorDE(a):
+            return a / 1.23785
+            
+
+def convertorED(a):
+            return a / 0.807852325
+            
+currency()

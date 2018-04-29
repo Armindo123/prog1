6b4e451a76c6f63022fe9e558d9aaae5af2c1541
Carlos <l.l_c@live.com.pt>
2018-04-29 15:24:45 +0100

    ex4 inc

---
+++ aula/fp6/aula6ex1-4.py
@@ -0,0 +1,10 @@
+#duas listas
+A = []
+B = []
+for _ in range(1, 6):
+    numA = int(input("Introduza um numero para a lista A: "))
+    A.append(numA)
+
+for _ in range(1, 6):
+    numB = int(input("Introduza um numero para a lista B: "))
+    B.append(numB)
